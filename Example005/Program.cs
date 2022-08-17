65b2d3823fd0a3b469549d611e32e882750bb123
Olesya <Olesyaholod@mail.ru>
2022-08-17 15:38:55 +0300

    Привет

---
+++ Example005/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Олеся")
+{
+    Console.WriteLine("Ура, это же ОЛЕСЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+*/
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "олеся")
+{
+    Console.WriteLine("Ура, это же ОЛЕСЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
